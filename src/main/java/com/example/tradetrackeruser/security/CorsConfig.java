4d8427e34a300760368b51b5b0dd5ab204e54475
Ms-You <msu122005@hs.ac.kr>
2025-01-22 21:37:51 +0900

    refactor: msa에서 Cors를 위해 User 서버에서는 적용 해제

--- src/main/java/com/example/tradetrackeruser/security/CorsConfig.java
+++ src/main/java/com/example/tradetrackeruser/security/CorsConfig.java
@@ -1,31 +1,31 @@
-package com.example.tradetrackeruser.security;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    @Primary
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-
-        corsConfig.addExposedHeader("Authorization");
-        corsConfig.addExposedHeader("RefreshToken");
-        corsConfig.addAllowedOriginPattern("http://localhost:3000");
-        corsConfig.addAllowedHeader("*");
-        corsConfig.addAllowedMethod("*");
-        corsConfig.setAllowCredentials(true);
-
-        // URL 패턴에 따라 CORS 설정을 설정하는 객체
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return source;
-    }
-}
+//package com.example.tradetrackeruser.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Primary;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.CorsConfigurationSource;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    @Primary
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration corsConfig = new CorsConfiguration();
+//
+//        corsConfig.addExposedHeader("Authorization");
+//        corsConfig.addExposedHeader("RefreshToken");
+//        corsConfig.addAllowedOriginPattern("http://localhost:3000");
+//        corsConfig.addAllowedHeader("*");
+//        corsConfig.addAllowedMethod("*");
+//        corsConfig.setAllowCredentials(true);
+//
+//        // URL 패턴에 따라 CORS 설정을 설정하는 객체
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfig);
+//
+//        return source;
+//    }
+//}
